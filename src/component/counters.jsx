27a5ae342d45c7252ab5231341ff0e95ad840d7a
SmartDell <myqy480@gmail.com>
2020-01-11 18:37:30 +0800

    forgot to saving a file >.<

--- src/component/counters.jsx
+++ src/component/counters.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Counter from './counter'
 class Counters extends React.Component {
-  render () {
+  render() {
     console.log('Counters-Rendered')
     return (
       <div>
